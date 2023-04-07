822c9ae5f32a97e8b9d2b9314ee1b31be42787a3
mikerogers04 <mikerogers04@hotmail.com>
2023-04-07 16:11:38 -0700

    Adds delete button, routing, and delete functionality.

--- src/pages/DinosaurShow.js
+++ src/pages/DinosaurShow.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
-import {Card, CardBody, CardSubtitle, CardText, CardTitle} from 'reactstrap'
+import { useParams, NavLink } from 'react-router-dom'
+import {Card, CardBody, CardSubtitle, CardText, CardTitle, Button, } from 'reactstrap'
 
-const DinoasaurShow = ({ dinosaurs }) => {
+const DinoasaurShow = ({ dinosaurs, deleteDinosaur }) => {
   const { id } = useParams()
-
-  let selectedDinosaur = mockDinosaurs.find(dinosaur => dinosaur.id === +id)
- 
+  let selectedDinosaur = dinosaurs.find(dinosaur => dinosaur.id === +id)
   return (
     <>
       <div>DinoasaurShow</div>
@@ -41,6 +39,16 @@ const DinoasaurShow = ({ dinosaurs }) => {
               <CardText>
                 Enjoys: {selectedDinosaur.enjoys}
               </CardText>
+              <Button>
+                  <NavLink to={`/dinosauredit/${selectedDinosaur.id}`}>
+                    Change the Fossil Record
+                  </NavLink>
+              </Button>
+              <Button onClick={deleteDinosaur}>
+                  <NavLink to={"/dinosaurindex/"}>
+                    Delete the Fossil Record
+                  </NavLink>
+              </Button>
             </CardBody>
           </Card>
         )}
